a4f142ca4d387bdef8443e108b3eedebe12c1f3f
nvquy <ngoq753429@gmail.com>
2023-03-27 16:56:19 +0700

    upate todo list

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+// Phép gán
+// Khai báo 
+// Biến
+// Kiểu dữ liệu
+//  
+    // trường hợp 1: Khi là hai số
+    let number1 =  10;
+    // number1: gọi là biến 
+    // let là từ khóa để khai báo biến
+    // = gọi là phép gán
+    let number2  =20;
+
+    let sum =number1 + number2;
+    // sum gọi là biến lưu trữ
+
+    // trường hợp 2: khi số thứ nhất là chuỗi, số thứ hai là số
+        // Thực hiện việc nối chuỗi
+    let number3 = '10';
+    let number4 = 20;
+    let number8 = 12;
+
+    console.log(number3+number4+number8);
+    
+    // trường hợp 3: Khi số thứ nhất là số, vế 2 là chuỗi
+    let number5 = 20;
+    let number6 = 30;
+    let number7 = '10';
+    // khai báo chuỗi có 2 cách: "", ''
+
+    console.log(number5 + number6 + number7);
